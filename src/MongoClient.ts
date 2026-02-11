25d69e4914c07e037eddc36dd0cac8c586bdce49
“Bartlomiej_Bombelka“ <b.bombelka@gmail.com>
2026-02-11 11:50:45 +0100

    fix typing

--- src/MongoClient.ts
+++ src/MongoClient.ts
@@ -16,10 +16,14 @@ let clientPromise: Promise<MongoClient>;
 if (process.env.NODE_ENV === "development") {
   // In development mode, use a global variable so changes are not lost
   // between hot reloads (important for Next.js dev server)
+
+  // @ts-ignore
   if (!global._mongoClientPromise) {
     client = new MongoClient(uri, options);
+    // @ts-ignore
     global._mongoClientPromise = client.connect();
   }
+  // @ts-ignore
   clientPromise = global._mongoClientPromise;
 } else {
   // In production (build & runtime on Vercel), always create fresh but still cache per instance
