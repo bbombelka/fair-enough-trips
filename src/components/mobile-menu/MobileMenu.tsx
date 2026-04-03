da2a9bd127dcbd86d077855db2f948297ad05540
“Bartlomiej_Bombelka“ <b.bombelka@gmail.com>
2026-04-03 19:40:35 +0200

    remove search from mobile menu, fix close mechanism

--- src/components/mobile-menu/MobileMenu.tsx
+++ src/components/mobile-menu/MobileMenu.tsx
@@ -20,13 +20,18 @@ export const MobileMenu = () => {
       {isOpen && (
         <div className={styles.menu}>
           <div className={styles["menu-bar"]}>
-            <Link href="/search" name={<FontAwesomeIcon className={styles["search-icon"]} icon={faSearch} fontSize={24} />}></Link>
+            <Link
+              href="/search"
+              name={<FontAwesomeIcon className={styles["search-icon"]} icon={faSearch} fontSize={24} onClick={() => setOpen(false)} />}
+            ></Link>
             <FontAwesomeIcon className={styles["close-icon"]} icon={faXmark} fontSize={28} onClick={() => setOpen(false)} />
           </div>
           <ul className={styles["menu-list"]}>
-            {menuLinks.map(({ name, href, nestedLinks }, i) => (
-              <MobileMenuItem key={i} href={href} name={name} nestedItems={nestedLinks} onClickCallback={() => setOpen(false)} />
-            ))}
+            {menuLinks
+              .filter(({ name }) => name !== "search") // search link icon is already in the menu heading
+              .map(({ name, href, nestedLinks }, i) => (
+                <MobileMenuItem key={i} href={href} name={name} nestedItems={nestedLinks} onClickCallback={() => setOpen(false)} />
+              ))}
           </ul>
         </div>
       )}
