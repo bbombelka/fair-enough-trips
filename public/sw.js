e5e08e438503992191600cf1d1daa01a37b0fb33
“Bartlomiej_Bombelka“ <b.bombelka@gmail.com>
2026-04-30 16:16:26 +0200

    allow caching of opaque responses

--- public/sw.js
+++ public/sw.js
@@ -4,7 +4,7 @@ self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(IMAGE_CACHE).then((cache) => {
       console.log(`Sw: ${cache} opened`);
-    })
+    }),
   );
 });
 
@@ -16,37 +16,42 @@ self.addEventListener("activate", (event) => {
           console.log(`SW: clearing ${key} cache`);
           caches.delete(key);
         }
-      })
-    )
+      }),
+    ),
   );
 });
 
 self.addEventListener("fetch", (event) => {
   if (event.request.destination === "image") {
-    caches
-      .open(IMAGE_CACHE)
-      .then(async (cache) => {
-        const cachedResponse = await cache.match(event.request);
+    event.respondWith(
+      caches
+        .open(IMAGE_CACHE)
+        .then(async (cache) => {
+          const cachedResponse = await cache.match(event.request);
 
-        if (cachedResponse) {
-          console.log("SW: returning cached response");
-          return cachedResponse;
-        }
-
-        return fetch(event.request.clone()).then((response) => {
-          if (response.status < 400 && response.headers.has("content-type") && response.headers.get("content-type").match(/^image\//i)) {
-            console.log("SW: caching response");
-            cache.put(event.request, response.clone());
+          if (cachedResponse) {
+            console.log("SW: returning cached response");
+            return cachedResponse;
           }
 
-          console.log("SW: returning fetched response");
-          return response;
-        });
-      })
-      .catch((error) => {
-        console.error("  Error in fetch handler:", error);
+          return fetch(event.request.clone()).then((response) => {
+            const isOpaque = response.type === "opaque" || response.status === 0;
+            const isImage = response.headers.has("content-type") && response.headers.get("content-type").match(/^image\//i);
 
-        throw error;
-      });
+            if ((response.status > 0 && response.status < 400 && isImage) || isOpaque) {
+              console.log("SW: caching response");
+              cache.put(event.request, response.clone());
+            }
+
+            console.log("SW: returning fetched response");
+            return response;
+          });
+        })
+        .catch((error) => {
+          console.error("  Error in fetch handler:", error);
+
+          throw error;
+        }),
+    );
   }
 });
