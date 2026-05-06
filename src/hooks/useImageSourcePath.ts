73b26ef0931a1450fa1b58f08fc870196f48ce29
“Bartlomiej_Bombelka“ <b.bombelka@gmail.com>
2026-05-06 16:45:36 +0200

    change image source

--- src/hooks/useImageSourcePath.ts
+++ src/hooks/useImageSourcePath.ts
@@ -5,13 +5,14 @@ export function useImageSourcePath() {
     src: string;
     thumbSrc: string;
   } {
-    const isProd = process.env.NODE_ENV === "production";
+    // const isProd = process.env.NODE_ENV === "production";
+    const isProd = true;
     const isNewPostDevMode = process.env.NEXT_PUBLIC_NEW_POST_ID;
 
     // const isProd = true;
     function getProdParams() {
-      const src = `${Config.S3_BUCKET}/posts/${id}/${filename}.${Config.DEFAULT_IMAGE_EXTENSION}`;
-      const thumbSrc = `${Config.S3_BUCKET}/posts/${id}/${filename}-thumb.${Config.DEFAULT_IMAGE_EXTENSION}`;
+      const src = `${Config.S3_BUCKET}/posts-v2/${id}/${filename}.${Config.DEFAULT_IMAGE_EXTENSION}`;
+      const thumbSrc = `${Config.S3_BUCKET}/posts-v2/${id}/${filename}-thumb.${Config.DEFAULT_IMAGE_EXTENSION}`;
 
       return { src, thumbSrc };
     }
