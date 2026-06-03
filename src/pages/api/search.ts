06e213dbade51d36beba2ff82e8258d13ea6e0c2
“Bartlomiej_Bombelka“ <b.bombelka@gmail.com>
2026-06-03 17:51:56 +0200

    skip subposts on search

--- src/pages/api/search.ts
+++ src/pages/api/search.ts
@@ -13,12 +13,17 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
     const ID_QUERY = searchQueryBuilder("id", searchTermRegex);
     const DESCRIPTION_QUERY = searchQueryBuilder("shortDescription", searchTermRegex);
     const LOGICAL_OPERATOR_OR = { $or: [TITLE_QUERY, ID_QUERY, DESCRIPTION_QUERY] };
+    const SKIP_SUBPOSTS = { parentId: { $exists: false } };
 
-    const latestPosts = await getLatestPosts(LOGICAL_OPERATOR_OR, 25);
+    const latestPosts = await getLatestPosts({ ...LOGICAL_OPERATOR_OR, ...SKIP_SUBPOSTS }, 25);
 
     // Strip category for search response to reduce payload, search component only needs a few fields
     const searchResults = latestPosts.map(({ id, title, postDate, base64Image, category }) => ({
-      id, title, postDate, base64Image, category
+      id,
+      title,
+      postDate,
+      base64Image,
+      category,
     }));
 
     return res.status(200).json(searchResults as any);
