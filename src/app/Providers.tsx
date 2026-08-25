3afb390195a5920552ff6e8c8798d0d16bfaca83
“Bartlomiej_Bombelka“ <b.bombelka@gmail.com>
2026-08-25 11:32:17 +0200

    remove autoscroll

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,20 +1,14 @@
 "use client";
 
-import { PropsWithChildren, useEffect } from "react";
-import { usePathname } from "next/navigation";
+import { PropsWithChildren } from "react";
+
 import { GlobalContextController } from "context/global/GlobalContextController";
 import { useSetGlobalStyleProperties } from "hooks/useSetGlobalStyleProperties";
 import { useServiceWorker } from "hooks/useServiceWorker";
 
 export function Providers({ children }: PropsWithChildren<{}>) {
-  const pathname = usePathname();
-
   useSetGlobalStyleProperties();
   useServiceWorker();
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
 
   return <GlobalContextController>{children}</GlobalContextController>;
 }
